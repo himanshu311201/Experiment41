128f35c0d28c5f0887734be4e3a932d8ca92c870
himanshu311201 <80580114+himanshu311201@users.noreply.github.com>
2021-09-27 19:22:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function validateForm() {
+// Validation
+console.log('Hello');
+const formComponent = [
+document.getElementById('tagline').value,
+document.getElementById('color').value,
+document.getElementById('address').value,
+document.getElementById('size').value,
+document.getElementById('date').value,
+];
+var j=0;
+for(var i=0;i<=5 && j!=1;i++)
+{
+if (formComponent[i]==="") {
+	j=1;
+alert("Please fill the details");
+return;
+}
+}
+// Bill
+const reciept1 = `RECEIPT \nTagline: ${formComponent[0]}\nColor:
+${formComponent[1]}\nAddress: ${formComponent[2]}\nSize:${formComponent[3]} \nDate:${formComponent[4]}`;
+confirm(reciept1);
+}
+const form = document.getElementById("buyForm");
+console.log(form)
+
+form.addEventListener("submit", (event) => {
+// stop form submission
+event.preventDefault();
+});
+
